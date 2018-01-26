4ed79cb98c7bb6806839c8bea971cfe6915da82d
Guido van Tricht <guido.van.tricht@valtech.nl>
2018-01-26 23:39:12 +0100

    Add project files.

---
+++ ExchangeChecker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExchangeChecker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
